7680ca837f4af58a149a88ff245a83b7ba5204cc
alexmeredith97 <alexmeredith97@gmail.com>
2015-12-10 12:25:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var U = 16
 var G = 9.8
 Magic();
 function Clear(){
-      canvas.width = canvas.width;
+      var c=document.getElementById("myCanvas");
+      var ctx=c.getContext("2d");
+      ctx.clearRect(0,0,c.width, c.height);
 }
 function updateAngleInput(val) {
       document.getElementById('angleInput').value=val; 
